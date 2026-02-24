47f2b25a78d327463b42988cb4f24d3c3e8a793b
ZPeikani <z.peikani23@gmail.com>
2026-02-24 09:30:27 +0330

    fix: main project

---
+++ src/theme/theme.tsx
@@ -0,0 +1,13 @@
+const theme = {
+  typography: {
+    h1: {
+      fontSize: "2.25rem",
+      fontWeight: "700",
+    },
+    body1: {
+      fontSize: "1rem",
+    },
+  },
+};
+
+export default theme;
