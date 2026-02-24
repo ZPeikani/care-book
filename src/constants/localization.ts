bd9d11ae01f6d14d8c372e0d1f5d84672a60deba
ZPeikani <z.peikani23@gmail.com>
2026-02-24 09:58:01 +0330

    create: localization

---
+++ src/constants/localization.ts
@@ -0,0 +1,10 @@
+export const BASE_URL = "http://localhost:3000";
+
+export const texts = {
+  signUp: "ثبت نام",
+  signIn: "ورود",
+  userName: "نام کاربری",
+  password: "رمز عبور",
+  email: "ایمیل",
+  register: "تایید",
+};
