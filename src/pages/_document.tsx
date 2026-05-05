b5b676194ff49853b2631e3360c7b95a65119fc2
ZPeikani <z.peikani23@gmail.com>
2026-05-05 23:22:32 +0430

    edit title tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,10 @@
+/* eslint-disable @next/next/no-title-in-document-head */
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en" dir="rtl">
       <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Vazirmatn:wght@400;500;700&display=swap"
-          rel="stylesheet"
-        />
         <title>CareBook</title>
       </Head>
       <body>
