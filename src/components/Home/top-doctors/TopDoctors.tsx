b3f43f8d028dee2b99d21504549d5b2d03eb5cd6
ZPeikani <z.peikani23@gmail.com>
2026-05-30 23:35:16 +0430

    add content box for top doctors

--- src/components/Home/top-doctors/TopDoctors.tsx
+++ src/components/Home/top-doctors/TopDoctors.tsx
@@ -1,10 +1,19 @@
 import { texts } from "@/constants/localization";
 import SectionTitle from "../section-title/SectionTitle";
+import Link from "next/link";
 
 export default function TopDoctors() {
   return (
     <div className="flex flex-col gap-8">
       <SectionTitle title={texts.topDoctors} fontSize="text-2xl" />
+      <div className="box shadow-md rounded-md bg-white p-4">
+        <div className="flex items-center">
+          <Link href={"/doctors"} className="mr-auto text-primary text-lg">
+            مشاهده همه
+          </Link>
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 }
