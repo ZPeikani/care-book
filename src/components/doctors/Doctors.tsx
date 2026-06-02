a5c1f5d020fc44b1e1b00fb03c5dc73de0f43c96
ZPeikani <z.peikani23@gmail.com>
2026-06-03 01:41:23 +0430

    build doctor's component

--- src/components/doctors/Doctors.tsx
+++ src/components/doctors/Doctors.tsx
@@ -1,7 +1,29 @@
+import { texts } from "@/constants/localization";
+import SearchBar from "../Home/search-bar/SearchBar";
+import BreadCrumbs from "../shared/breadcrumbs/Breadcrumbs";
+import CardDoctor from "../card/card-doctor/CardDoctor";
+
 function Doctors() {
   return (
     <>
-      <div>Doctors</div>
+      <div className="flex flex-col my-8 gap-8">
+        <BreadCrumbs array={[[texts.home, "/"], [texts.doctors]]} />
+        <SearchBar display="hidden md:block" />
+        <div className="grid grid-cols-2 md:grid-cols-4 sm:grid-cols-3 gap-6 items-center">
+          <CardDoctor />
+          <CardDoctor />
+          <CardDoctor />
+          <CardDoctor />
+          <CardDoctor />
+          <CardDoctor />
+          <CardDoctor />
+          <CardDoctor />
+          <CardDoctor />
+          <CardDoctor />
+          <CardDoctor />
+          <CardDoctor />
+        </div>
+      </div>
     </>
   );
 }
