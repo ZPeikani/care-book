38afa4a012c3dd941e21df0766be09d5f27630c8
ZPeikani <z.peikani23@gmail.com>
2026-06-03 01:39:51 +0430

    create doctor's card

---
+++ src/components/card/card-doctor/CardDoctor.tsx
@@ -0,0 +1,42 @@
+import drRamezani from "@/assets/doctors/dr-ramezani.jpg";
+import Buttons from "@/components/shared/buttons/Buttons";
+import { StaticImageData } from "next/image";
+type Doctor = {
+  id: number;
+  name: string;
+  specialty: string;
+  image: StaticImageData;
+};
+
+export default function CardDoctor() {
+  const doctor: Doctor = {
+    id: 1,
+    name: "دکتر مجید رمضانی",
+    specialty: "متخصص غدد",
+    image: drRamezani,
+  };
+  return (
+    <div>
+      <div className="relative max-w-3xs w-full py-4 border border-gray-300 rounded-xl shadow-md">
+        <div className="flex flex-col items-center">
+          <img
+            className="w-24 h-24 mb-6 rounded-full"
+            src={doctor.image.src}
+            alt={doctor.name}
+          />
+          <h5 className="mb-0.5 text-lg tracking-tight text-heading">
+            {doctor.name}
+          </h5>
+          <span className="text-sm text-gray-500">{doctor.specialty}</span>
+          <div className="flex mt-2 md:mt-2 gap-4">
+            <Buttons
+              buttonType="button"
+              buttonText="نوبت بگیرید"
+              classNames="px-2"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
