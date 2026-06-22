37b27160b82d8ed80aea7038ad16386ec6c67f1e
ZPeikani <z.peikani23@gmail.com>
2026-06-23 01:32:04 +0430

    create doctors array

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -4,17 +4,33 @@ import Specialties from "./specialties/Specialties";
 import TopDoctors from "./top-doctors/TopDoctors";
 import bannerContactUs from "@/assets/poster/more-info.png";
 import bannerDoctorRegister from "@/assets/poster/doctor-register.png";
+import drRamezani from "@/assets/doctors/dr-ramezani.jpg";
+import drTaheri from "@/assets/doctors/dr-taheri.webp";
 import CareBlogs from "./blogs/CareBlogs";
 export default function Home() {
+  const doctors = [
+    {
+      id: 1,
+      name: "دکتر مجید رمضانی",
+      specialty: "متخصص غدد",
+      image: drRamezani,
+    },
+    {
+      id: 2,
+      name: "دکتر حسن طاهری",
+      specialty: "تخصص ارتوپدی",
+      image: drTaheri,
+    },
+  ];
   return (
     <div className="mb-10 flex flex-col gap-16">
       <div>
         <img src={banner.src} alt="banner" />
-        <SearchBar display="hidden md:block" />
+        <SearchBar display="hidden md:block" doctors={doctors} />
       </div>
       <Specialties />
-      <img src={bannerContactUs.src} alt="bannerContactUs"/>
-      <TopDoctors />
+      <img src={bannerContactUs.src} alt="bannerContactUs" />
+      <TopDoctors doctors={doctors} />
       <CareBlogs />
       <img
         src={bannerDoctorRegister.src}
