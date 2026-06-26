c4235e236cc08254aa24d5a023a8d781850ce40f
ZPeikani <z.peikani23@gmail.com>
2026-06-26 23:41:57 +0430

    fixed: add card doctor with map

--- src/components/doctors/Doctors.tsx
+++ src/components/doctors/Doctors.tsx
@@ -1,27 +1,19 @@
 import { texts } from "@/constants/localization";
-import SearchBar from "../Home/search-bar/SearchBar";
-import BreadCrumbs from "../shared/breadcrumbs/Breadcrumbs";
-import CardDoctor from "../card/card-doctor/CardDoctor";
+import SearchBar from "@/components/Home/search-bar/SearchBar";
+import BreadCrumbs from "@/components/shared/breadcrumbs/Breadcrumbs";
+import CardDoctor from "@/components/card/card-doctor/CardDoctor";
+import { doctors } from "@/components/Home/Home";
 
 function Doctors() {
   return (
     <>
       <div className="flex flex-col my-8 gap-8">
         <BreadCrumbs array={[[texts.home, "/"], [texts.doctors]]} />
-        <SearchBar display="hidden md:block" />
+        <SearchBar display="hidden md:block" doctors={doctors} />
         <div className="grid grid-cols-2 md:grid-cols-4 sm:grid-cols-3 gap-6 items-center">
-          <CardDoctor />
-          <CardDoctor />
-          <CardDoctor />
-          <CardDoctor />
-          <CardDoctor />
-          <CardDoctor />
-          <CardDoctor />
-          <CardDoctor />
-          <CardDoctor />
-          <CardDoctor />
-          <CardDoctor />
-          <CardDoctor />
+          {doctors.map(doctor => (
+          <CardDoctor key={doctor.id} doctor={doctor} />
+        ))}
         </div>
       </div>
     </>
