fc25a7fc83e8746596f4c797722536d26180801c
ZPeikani <z.peikani23@gmail.com>
2026-06-26 23:40:00 +0430

    create: card searchbar

---
+++ src/components/card/card-searchbar/CardSearchbar.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import { StaticImageData } from "next/image";
+
+type Doctor = {
+  id: number;
+  name: string;
+  specialty: string;
+  image: StaticImageData;
+};
+type Props = {
+  doctor: Doctor;
+};
+
+export default function CardSearchbar({ doctor }: Props) {
+  return (
+    <>
+    <Link href={`/doctors/${doctor.id}`} className="block relative w-full p-1 border-b border-gray-300 hover:bg-blue-50" onMouseDown={(e) => e.preventDefault()}>
+        <div className="flex gap-4 items-center mb-1">
+          <img
+            className="w-14 h-14 rounded-full"
+            src={doctor.image.src}
+            alt={doctor.name}
+          />
+        <div className="items-center">
+          <p className="text-base tracking-tight">
+            {doctor.name}
+          </p>
+          <span className="text-sm text-gray-500">{doctor.specialty}</span>
+         </div>
+        </div>
+      </Link>
+    </>
+  );
+}
