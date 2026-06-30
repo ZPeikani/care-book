492972d1dd76d3f3b11be680dd74b2b58451762f
ZPeikani <z.peikani23@gmail.com>
2026-07-01 02:01:51 +0430

    separate data from component

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -4,23 +4,9 @@ import Specialties from "./specialties/Specialties";
 import TopDoctors from "./top-doctors/TopDoctors";
 import bannerContactUs from "@/assets/poster/more-info.png";
 import bannerDoctorRegister from "@/assets/poster/doctor-register.png";
-import drRamezani from "@/assets/doctors/dr-ramezani.jpg";
-import drTaheri from "@/assets/doctors/dr-taheri.webp";
 import CareBlogs from "./blogs/CareBlogs";
-export const doctors = [
-  {
-    id: 1,
-    name: "دکتر مجید رمضانی",
-    specialty: "متخصص غدد",
-    image: drRamezani,
-  },
-  {
-    id: 2,
-    name: "دکتر حسن طاهری",
-    specialty: "تخصص ارتوپدی",
-    image: drTaheri,
-  },
-];
+import { doctors } from "@/data/doctors";
+
 export default function Home() {
   return (
     <div className="mb-10 flex flex-col gap-16">
